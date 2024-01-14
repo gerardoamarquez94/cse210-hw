a2ffdb469fb22bb13ecfb89ce6d4297281770458
gerardoamarquez94 <145737951+gerardoamarquez94@users.noreply.github.com>
2024-01-14 02:09:02 -0600

    Update Program.cs

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+class NumberProcessing
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (true)
+        {
+            Console.Write("Enter number: ");
+            int number = int.Parse(Console.ReadLine());
+            if (number == 0)
+                break;
+            numbers.Add(number);
+        }
+
+        int sum = 0, max = Int32.MinValue, smallestPositive = Int32.MaxValue;
+        foreach (int num in numbers)
+        {
+            sum += num;
+            if (num > max)
+                max = num;
+            if (num > 0 && num < smallestPositive)
+                smallestPositive = num;
+        }
+
+        double average = numbers.Count > 0 ? (double)sum / numbers.Count : 0;
+        
+        numbers.Sort();
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {max}");
+        Console.WriteLine($"The smallest positive number is: {smallestPositive}");
+        Console.WriteLine("The sorted list is:");
+        foreach (int num in numbers)
+        {
+            Console.WriteLine(num);
+        }
     }
 }
