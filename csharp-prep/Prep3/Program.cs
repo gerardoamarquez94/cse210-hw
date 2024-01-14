df2adcc54168dc05c28034c8b694446da0f31111
gerardoamarquez94 <145737951+gerardoamarquez94@users.noreply.github.com>
2024-01-14 02:07:48 -0600

    Update Program.cs

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,46 @@
 using System;
 
-class Program
+class GuessMyNumberGame
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep3 World!");
+        bool playAgain = true;
+
+        while (playAgain)
+        {
+            // Generate a random magic number
+            Random random = new Random();
+            int magicNumber = random.Next(1, 101);
+            int userGuess = 0;
+            int guessCount = 0;
+
+            // Loop until the user guesses the magic number
+            while (userGuess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                userGuess = int.Parse(Console.ReadLine());
+                guessCount++;
+
+                if (userGuess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (userGuess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it in {guessCount} guesses!");
+                }
+            }
+
+            // Ask the user if they want to play again
+            Console.Write("Do you want to play again? (yes/no): ");
+            string response = Console.ReadLine().Trim().ToLower();
+            playAgain = response == "yes";
+        }
+
+        Console.WriteLine("Thank you for playing!");
     }
 }
