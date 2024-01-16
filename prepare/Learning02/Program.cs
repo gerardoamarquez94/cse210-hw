9f50ece6aed1d08a8cf084273b86ca6f62bc7d4a
gerardoamarquez94 <145737951+gerardoamarquez94@users.noreply.github.com>
2024-01-16 11:15:35 -0600

    Update Program.cs

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+public class Job
 {
-    static void Main(string[] args)
+    public string Company { get; set; }
+    public string JobTitle { get; set; }
+    public int StartYear { get; set; }
+    public int EndYear { get; set; }
+
+    public override string ToString()
     {
-        Console.WriteLine("Hello Learning02 World!");
+        return $"{JobTitle} ({Company}) {StartYear}-{EndYear}";
+    }
+}
+
+public class Resume
+{
+    public string Name { get; set; }
+    public List<Job> Jobs { get; set; }
+
+    public Resume()
+    {
+        Jobs = new List<Job>();
+    }
+
+    public void DisplayResume()
+    {
+        Console.WriteLine(Name);
+        foreach (var job in Jobs)
+        {
+            Console.WriteLine(job.ToString());
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        var job1 = new Job { Company = "Microsoft", JobTitle = "Software Engineer", StartYear = 2019, EndYear = 2022 };
+        var job2 = new Job { Company = "Google", JobTitle = "Senior Software Engineer", StartYear = 2022, EndYear = 2024 };
+
+        var resume = new Resume { Name = "John Doe" };
+        resume.Jobs.Add(job1);
+        resume.Jobs.Add(job2);
+
+        resume.DisplayResume();
     }
 }
